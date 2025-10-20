b026fcb476aac96b81fd1f8e3735937766080872
Luke_sinclair <Lukesin@outlook.com>
2025-10-20 13:46:59 +0200

    sql code

---
+++ SQL_Commands/sql commands.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Produkts (
+    produktID int not null AUTO_INCREMENT,
+    pris float not null,
+    navn varchar(55) not null,
+    PRimary key (produktID)
+);
+
+CREATE TABLE lagers(
+    lagerID int not null AUTO_INCREMENT,
+    navn varchar(55) not null,
+    PRimary key (lagerID)
+);
+
+CREATE TABLE lager_manger(
+    lagermangerID int not null AUTO_INCREMENT,
+    lagerID int not null,
+    produktID int not null,
+    antal int,
+    PRimary key (lagermangerID),
+    
+    FOREIGN KEY (lagerID) REFERENCES lagers(lagerID),
+    FOREIGN KEY (produktID) REFERENCES Produkts(produktID)
+);
+create table customers(
+	customerid int not null AUTO_INCREMENT,
+    navn varchar(255) not null,
+    email varchar(255) not null,
+    PRimary key (customerid)
+
+);
+create table orders (
+	OrderID int not null AUTO_INCREMENT,
+    produktID int not null,
+    invoicenummer int not null,
+    customerid int not null,
+    status varchar(55),
+    mængde int not null,
+    PRimary key (OrderID),
+    
+    FOREIGN KEY (produktID) REFERENCES Produkts(produktID),
+    FOREIGN KEY (customerid) REFERENCES customers(customerid)
+);
+
+create table admin(
+	adminid int not null AUTO_INCREMENT,
+    navn varchar(255),
+    adminpassword varchar(255),
+    
+    primary key (adminid)
+);
