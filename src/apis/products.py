ee16ccf24ba3e32a042608dcceee848de447e16f
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-22 14:53:17 +0200

    Added data model for product

--- src/apis/products.py
+++ src/apis/products.py
@@ -1,21 +1,22 @@
 from flask_restx import Namespace, Resource, fields, Model
 from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
-from auth import authorizations
+from apis.auth import authorizations
 
 api: Namespace = Namespace("product", description="Product namespace", authorizations=authorizations)
 
 product_list = list(("aluminium", "banana", "apple", "car"))
 
 get_product_model: Model = api.model('ProductModel', {
-        'ID': fields.String(required=True, description='ID of product')})
+        'ID': fields.Integer(required=True, description='ID of product')})
 
 @api.route("/product")
 class Product(Resource):
     @api.doc('Get product based on ID')
     @api.expect(get_product_model)
     def get(self):
-        result = product_list[1]
+        ID = api.payload('ID')
+        result = product_list[ID]
         return jsonify({'product': result})
     
     def put(self, product):
