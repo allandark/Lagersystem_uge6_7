ad1ac48c69d0ef62be5a0b58f5627d6c47246c8f
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-23 11:04:33 +0200

    Update customer.py

--- src/apis/customer.py
+++ src/apis/customer.py
@@ -7,14 +7,14 @@ api: Namespace = Namespace("customer", description="Customer namespace", authori
 
 customer_dict = dict({1 : "Viktor", 2 : "Dennis", 3: "Luke"})
 
-#get_customer_model: Model = api.model('Get customer model', {'id': fields.Integer(required=True, description='Customer ID number')})
+get_customer_model: Model = api.model('Get customer model', {'id': fields.Integer(required=True, description='Customer ID number')})
 
 @api.route("/customer/")
 class Customer(Resource):
     
     @api.doc('Get the customer based on the ID number')
     #@api.expect(get_customer_model)
-    def get(self, id):
+    def get(self):
         #id = api.payload['id']
         #customer = customer_dict[id]
-        return jsonify({'message': customer_dict[id]})
+        return jsonify({'message': customer_dict[1]})
