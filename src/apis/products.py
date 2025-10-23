6de55de5d7bb3d89baa98f474fd5cc86de2123a4
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-23 09:01:49 +0200

    Update products.py

--- src/apis/products.py
+++ src/apis/products.py
@@ -7,14 +7,25 @@ api: Namespace = Namespace("product", description="Product namespace", authoriza
 
 product_list = list(("aluminium", "banana", "apple", "car"))
 
-new_product_model: Model = api.model('ProductModel', {
+get_product_model: Model = api.model('GetProductModel', {'id': fields.Integer(required=True, description='ID of product')})
+
+new_product_model: Model = api.model('NewProductModel', {
         'name': fields.String(required=True, description='Name of product')})
+
+remove_product_model: Model = api.model('RemoveProductModel', {
+        'id': fields.Integer(required=True, description='Name of product')})
+
+update_product_model: Model = api.model('UpdateProductModel', {
+        'id': fields.Integer(required=True, description='Name of product'),
+        'status': fields.String(required=True, description='New status of product')})
 
 @api.route("/product")
 class Product(Resource):
     @api.doc('Get product based on ID')
+    @api.expect(get_product_model)
     def get(self):
-        return jsonify({'product': product_list})
+        id = api.payload['id']
+        return jsonify({'product': product_list[id]})
     
     @api.doc('Add new product')
     @api.expect(new_product_model)
@@ -23,10 +34,18 @@ class Product(Resource):
         product_list.append(product)
         return jsonify({'New product': product_list})
 
-    def delete(self, ID):
+    @api.doc('Remove product')
+    @api.expect(remove_product_model)
+    def delete(self):
+        ID = api.payload['id']
         result = product_list.pop(ID)
         return jsonify({'Removed product': result})
 
-    def post(self, ID, new_product):
-        product_list[ID] = new_product
+
+    @api.doc('Update product')
+    @api.expect(update_product_model)
+    def post(self):
+        ID = api.payload['id']
+        status = api.payload['status']
+        product_list[ID] = status
         return jsonify({'Updated product': product_list[ID]})
