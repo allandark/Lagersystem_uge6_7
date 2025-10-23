a438b5012bfda4fce8894e9dfc1a6aa4240e2ea1
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-23 10:46:24 +0200

    Update customer.py

--- src/apis/customer.py
+++ src/apis/customer.py
@@ -1,13 +1,20 @@
-from flask_restx import Namespace, Resource, fields, Model
+from flask_restx import Namespace, Resource, fields, Model, reqparse
 from flask import Flask, jsonify, request
 from flask_restx import Api, Resource
 from apis.auth import authorizations
 
 api: Namespace = Namespace("customer", description="Customer namespace", authorizations=authorizations)
 
+customer_dict = dict({1 : "Viktor", 2 : "Dennis", 3: "Luke"})
 
+#get_customer_model: Model = api.model('Get customer model', {'id': fields.Integer(required=True, description='Customer ID number')})
 
+@api.route("/customer/")
 class Customer(Resource):
     
-    def get(self):
-        return jsonify({'message': 'customer'})
+    @api.doc('Get the customer based on the ID number')
+    #@api.expect(get_customer_model)
+    def get(self, id):
+        #id = api.payload['id']
+        #customer = customer_dict[id]
+        return jsonify({'message': customer_dict[id]})
