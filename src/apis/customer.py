9e6dea6e6050bde22b0ce854bf0179756c2366bd
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-10-23 14:01:18 +0200

    Update customer.py

--- src/apis/customer.py
+++ src/apis/customer.py
@@ -5,16 +5,58 @@ from apis.auth import authorizations
 
 api: Namespace = Namespace("customer", description="Customer namespace", authorizations=authorizations)
 
-customer_dict = dict({1 : "Viktor", 2 : "Dennis", 3: "Luke"})
-
+customer_dict = [
+{"id":0, "name": "Dennis"},
+{"id":1, "name": "Viktor"},
+{"id":2, "name": "Luke"},
+]
 get_customer_model: Model = api.model('GetCustomerModel', {'id': fields.Integer(required=True, description='Customer ID number'),
                                         'name': fields.String(required=True, description='Name of customer')})
 
-@api.route("/customer/<int:id>&<string:name>")
-class Customer(Resource):
+new_customer_model: Model = api.model('NewCustomerModel', {'id': fields.Integer(required=True, description='Customer ID number'),
+                                        'name': fields.String(required=True, description='Name of customer')})
+
+update_customer_model: Model = api.model('UpdateCustomerModel', {'id': fields.Integer(required=True, description='Customer ID number'),
+                                        'new_name': fields.String(required=True, description='New name of customer')})
+
+remove_customer_model: Model = api.model('RemoveCustomerModel', {'id': fields.Integer(required=True, description='Customer ID number'),
+                                        'name': fields.String(required=True, description='New name of customer')})
+@api.route("/<int:id>&<string:name>")
+class CustomerGet(Resource):
     
     @api.doc('Get the customer based on the ID number')
     @api.marshal_with(get_customer_model, code = 200)
     def get(self, id, name):
         customer = customer_dict[id]
         return jsonify(customer), 200
+    
+@api.route("/new")
+class CustomerPost(Resource):
+    @api.doc('Add a new customer')
+    @api.expect(new_customer_model)
+    def post(self):
+        ID = api.payload['id']
+        name = api.payload['name']
+        customer_dict.append({"id": ID, "name": name}) 
+        return jsonify({'New customer': customer_dict[ID]})
+    
+@api.route("/update")
+class CustomerUpdate(Resource):
+
+    @api.doc('Update customer info')
+    @api.expect(update_customer_model)
+    def put(self):
+        ID = api.payload['id']
+        name = api.payload['new_name']
+        customer_dict[ID] = {"id": ID, "name": name}
+        return jsonify({"Updated customer": customer_dict[ID]})
+    
+@api.route("/remove")
+class CustomerDelete(Resource):
+
+    api.doc('Delete a customer')
+    api.expect(remove_customer_model)
+    def delete(self):
+        ID = api.payload['id']
+        customer_dict.pop(ID)
+        return jsonify({'Removed customer': customer_dict})
