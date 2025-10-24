379b2f777c620b81473db3f4afd13cf1e6a7108c
Dennis Amtoft Jensen <dennis.jensen@specialisterne.dk>
2025-10-24 10:52:09 +0200

    warehouse unittest complete, needs to be updated with the db interface in place

--- tests/unit/test_warehouse.py
+++ tests/unit/test_warehouse.py
@@ -166,7 +166,7 @@ def test_post_warehouse_inventory_no_auth(client, endpoint,
     ("/warehouse/0/inventory", {"id": 0, "product_id":0, "warehouse_id": 0, "quantity": 5}, 405),
     ("/warehouse/999/inventory/999", {"id": 0, "product_id":0, "warehouse_id": 0, "quantity": 5}, 404),
     ("/warehouse/0/inventory/abc", {"id": "abc", "product_id":"abc", "warehouse_id": "abc", "quantity": "abc"}, 404),    
-    ("/warehouse/0/inventory/", {}, 400)
+    ("/warehouse/0/inventory/", {}, 404)
 ])
 def test_put_warehouse_inventory_auth(client, endpoint, 
         payload, headers, expected_status, 
@@ -177,12 +177,47 @@ def test_put_warehouse_inventory_auth(client, endpoint,
             assert response.status_code == expected_status
 
 
-# def test_put_warehouse_inventory_no_auth(client, payload, expected_status):
-    # response = client.delete(endpoint)
-    # assert response.status_code == expected_status
-# def test_delete_warehouse_inventory_auth(client, payload, headers, expected_status):
-    # response = client.delete(endpoint, headers=headers)
-    # assert response.status_code == expected_status
-# def test_delete_warehouse_inventory_no_auth(client, payload, expected_status):
-    # response = client.delete(endpoint)
-    # assert response.status_code == expected_status
+@pytest.mark.parametrize("endpoint, payload, expected_status", [
+    ("/warehouse/0/inventory/0", {"id": 0, "product_id":0, "warehouse_id": 0, "quantity": 5}, 401),
+    ("/warehouse/0/inventory", {"id": 0, "product_id":0, "warehouse_id": 0, "quantity": 5}, 405),
+    ("/warehouse/999/inventory/999", {"id": 0, "product_id":0, "warehouse_id": 0, "quantity": 5}, 401),
+    ("/warehouse/0/inventory/abc", {"id": "abc", "product_id":"abc", "warehouse_id": "abc", "quantity": "abc"}, 404),    
+    ("/warehouse/0/inventory/", {}, 404)
+])
+def test_put_warehouse_inventory_no_auth(client, endpoint, 
+        payload, expected_status, 
+        mock_warehouses_data, mock_inventory_data):
+    with patch("apis.warehouse.warehouses", mock_warehouses_data): 
+        with patch("apis.warehouse.warehouse_items", mock_inventory_data): 
+            response = client.put(endpoint, json=payload)
+            assert response.status_code == expected_status
+
+@pytest.mark.parametrize("endpoint, expected_status", [
+    ("/warehouse/0/inventory/0", 200),
+    ("/warehouse/0/inventory/999", 404),
+    ("/warehouse/0/inventory/abc", 404),
+    ("/warehouse/999/inventory/0", 404),
+    ("/warehouse/abc/inventory/0", 404)
+])
+def test_delete_warehouse_inventory_auth(client, endpoint, 
+        expected_status, headers,
+        mock_warehouses_data, mock_inventory_data):
+    with patch("apis.warehouse.warehouses", mock_warehouses_data): 
+        with patch("apis.warehouse.warehouse_items", mock_inventory_data): 
+            response = client.delete(endpoint, headers=headers)
+            assert response.status_code == expected_status
+
+@pytest.mark.parametrize("endpoint, expected_status", [
+    ("/warehouse/0/inventory/0", 401),
+    ("/warehouse/0/inventory/999", 401),
+    ("/warehouse/0/inventory/abc", 404),
+    ("/warehouse/999/inventory/0", 401),
+    ("/warehouse/abc/inventory/0", 404)
+])
+def test_delete_warehouse_inventory_no_auth(client, endpoint, 
+        expected_status,
+        mock_warehouses_data, mock_inventory_data):
+    with patch("apis.warehouse.warehouses", mock_warehouses_data): 
+        with patch("apis.warehouse.warehouse_items", mock_inventory_data): 
+            response = client.delete(endpoint)
+            assert response.status_code == expected_status
