8c69d2f3f9d5b7566e6bf182f7e399b1093e55c3
Luke_sinclair <Lukesin@outlook.com>
2025-10-24 11:11:29 +0200

    added try and exception for all metods

--- src/Database_commands/product.py
+++ src/Database_commands/product.py
@@ -5,61 +5,84 @@ class ProductModel:
         self.db = db 
     
     def GetAll(self):
-
-        self.db.execute("SELECT * FROM produkts")
-
-        myresult = self.db.fetchall()
         
-        return myresult
+        try:
+            
+            self.db.execute("SELECT * FROM produkts")
+
+            myresult = self.db.fetchall()
+        
+            return myresult
+        except Exception as e:
+            print("Error getting product:", e)
 
     def GetbyId(self, id):
-
-        self.db.execute(f"SELECT * FROM produkts where produktID = %s ", (id,))
-
-        myresult = self.db.fetchall()
         
-        return myresult
+        try:
+            self.db.execute(f"SELECT * FROM produkts where produktID = %s ", (id,))
 
+            myresult = self.db.fetchall()
+        
+            return myresult
+        
+        except Exception as e:
+            print("Error getting product by id:", e)
 
     def GetbyPrise(self, pris):
-
-        self.db.execute(f"SELECT * FROM produkts where pris = %s ", (pris,))
-
-        myresult = self.db.fetchall()
         
-        return myresult
+        try:
+            self.db.execute(f"SELECT * FROM produkts where pris = %s ", (pris,))
+
+            myresult = self.db.fetchall()
+        
+            return myresult
+        except Exception as e:
+            print("Error getting product by price:", e)
 
     def GetPricebyiterval(self, lov_price,high_price):
-
-        self.db.execute(f"SELECT * FROM produkts WHERE pris BETWEEN %s AND %s" , (lov_price,high_price))
-
-        myresult = self.db.fetchall()
         
-        return myresult
+        try:
+            self.db.execute(f"SELECT * FROM produkts WHERE pris BETWEEN %s AND %s" , (lov_price,high_price))
 
+            myresult = self.db.fetchall()
+        
+            return myresult
+        except Exception as e:
+            print("Error getting product in price interval:", e)
+            
     def insertproduct(self, name, price):
+        
+        try:
+            query = "INSERT INTO produkts (navn, pris) VALUES (%s, %s)"
 
-        query = "INSERT INTO produkts (navn, pris) VALUES (%s, %s)"
-
-        self.db.execute(query, (name, price))
-        self.db.commit()
+            self.db.execute(query, (name, price))
+            self.db.commit()
+            return True
+        except Exception as e:
+            print("Error inserting product:", e)
+            return False
 
     def GetbyName(self, name):
-
-        self.db.execute(f"SELECT * FROM produkts where navn = %s ", (name,))
-
-        myresult = self.db.fetchall()
         
-        return myresult
+        try:
+            self.db.execute(f"SELECT * FROM produkts where navn = %s ", (name,))
+
+            myresult = self.db.fetchall()
+        
+            return myresult
+        except Exception as e:
+            print("Error getting product by name:", e)
 
     def exist(self,id):
+        try:
+            self.db.execute(f"SELECT exists(select 1 from produkts where produktID = %s)AS id_exists ", (id,))
 
-        self.db.execute(f"SELECT exists(select 1 from produkts where produktID = %s)AS id_exists ", (id,))
+            myresult = self.db.fetchall()
 
-        myresult = self.db.fetchall()
-
-        if myresult == [(0,)]:
-            return False
-        else:
-            return True
+            if myresult == [(0,)]:
+                return False
+            else:
+                return True
+        except Exception as e:
+            print("Error checking if product exist by id:", e)
 
