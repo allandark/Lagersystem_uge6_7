9a41131c3fb7e19ca2a1863d15b04842c3f67045
Luke_sinclair <Lukesin@outlook.com>
2025-10-24 11:02:02 +0200

    database_mangment small changes

--- src/Database_commands/database_mangment.py
+++ src/Database_commands/database_mangment.py
@@ -4,20 +4,26 @@ from product import ProductModel
 class Database_mangment:
     def __init__(self, host, user, password, dbName):
         
+        try:
+            self.mydb = connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=dbName
+            )
         
-        self.mydb = connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=dbName
-        )
+            self.product = ProductModel(self.mydb.cursor())
         
-        self.product = ProductModel(self.mydb.cursor())
+        finally:
+            if 'connection' in locals() and self.mydb.is_connected():
+                self.mydb.close()
         
-        
-        
-    def isconnect():
-        return True
+    def isconnect(self):
+        if self.mydb.is_connected():
+            return True
+        else:
+            return False
+
         
     
     
