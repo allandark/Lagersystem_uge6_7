202d10fca4d3b7e396a2642e4587f2db8851bec3
JamesWebbTelescope <112393315+JamesWebbTelescope@users.noreply.github.com>
2025-11-05 13:29:52 +0100

    Started writing unit tests for product.py

--- tests/unit/test_product.py
+++ tests/unit/test_product.py
@@ -1,5 +1,22 @@
 import pytest
+from unittest.mock import patch
+from flask_restx import Namespace, Resource, fields, Model
+from flask_jwt_extended import create_access_token
 
+from src.app import create_app
+from src.apis import api
 
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config['TESTING'] = True
+    with app.app_context():
+        with app.test_client() as client:        
+            yield client
+
+@pytest.fixture
+def headers():
+    token = create_access_token("testuser")
+    return {"Authorization": f"Bearer {token}"}
 # def test_demo():
 #     assert True
