0bb186fcb3b9bfd52c9eefbb7c4d98cedb770c1b
Dennis Amtoft Jensen <dennis.jensen@specialisterne.dk>
2025-11-07 10:11:01 +0100

    test fix

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pytest tests/unit --junitxml="tests/results/unittest_report.xml
+pytest tests/unit --junitxml="tests/results/unittest_report.xml"
 # pytest tests/integration --junitxml="tests/results/integrationtest_report.xml"
