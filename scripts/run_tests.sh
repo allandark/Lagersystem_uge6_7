2f6d9edd1766d5e9f32601c71be7cee70f87f1e7
Dennis Amtoft Jensen <dennis.jensen@specialisterne.dk>
2025-11-10 08:50:29 +0100

    fix test artifacts

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -f "tests/results/*.xml"
 pytest tests/unit --junitxml="tests/results/unittest_report.xml"
 ls -R tests
 # pytest tests/integration --junitxml="tests/results/integrationtest_report.xml"
